b924ffa7bac65ff034527916874cdb4d3719b7ea
DumisileM <dumisilem@aol.com>
2023-01-10 23:26:03 +0000

    completed logic so that the length of password matches the selected length and to make user select at least one character type

--- starter/script.js
+++ starter/script.js
@@ -93,35 +93,45 @@ function getPasswordOptions() {
   var userResponse = {};
   // ask user for length of password.
   while(true){
-    var passwordLength =parseInt(prompt("How long do you want your password to be? \n Must be at least 10 characters but no more than 64 characters."));
-    
-    
-    if (passwordLength >=10 && passwordLength <= 64 && Number.isInteger(passwordLength)){
-          userResponse['length']= passwordLength;
-          
-          break;
-      } else {
-        alert("Invalid input. Please enter a numerical value between 10-64");
-      };
+      var passwordLength =parseInt(prompt("How long do you want your password to be? \n Must be at least 10 characters but no more than 64 characters."));
+      
+      
+      if (passwordLength >=10 && passwordLength <= 64 && Number.isInteger(passwordLength)){
+            userResponse['length']= passwordLength;
+            
+            break;
+        } else {
+          alert("Invalid input. Please enter a numerical value between 10-64");
+        };
     
 
   };
+
+  console.log(Object.values(userResponse));
+
+  while(Object.values(userResponse).includes(true)==false){
+       
+       
+      //ask if they want any uppercase?
+      var wantUppercase= confirm("Do you want any uppercase letters in your password? \n Press OK if you DO and Cancel if you do not.");
+      userResponse["uppercase"] = wantUppercase;
+
+      //ask if they want any lowercase?
+      var wantLowercase= confirm("Do you want any lowercase letters in your password? \n Press OK if you DO and Cancel if you do not.");
+      userResponse["lowercase"] = wantLowercase;
+      //ask if they want any numeric values?
+      var wantNumeric= confirm("Do you want any numeric digits in your password? \n Press OK if you DO and Cancel if you do not.");
+      userResponse["numeric"] = wantNumeric;
+      //ask if they want any special characters?
+      var wantSpecialChar= confirm("Do you want any special characters($@%&*, etc) in your password? \n Press OK if you DO and Cancel if you do not.");
+      userResponse["specialChars"] = wantSpecialChar;
+      console.log(Object.values(userResponse).includes())
+      if( Object.values(userResponse).includes(true)==false){
+        alert('Please at least one character type');
+      };
+  }
+
   
-  //ask if they want any uppercase?
-  var wantUppercase= confirm("Do you want any uppercase letters in your password? \n Press OK if you DO and Cancel if you do not.");
-  userResponse["uppercase"] = wantUppercase;
-
-  //ask if they want any lowercase?
-  var wantLowercase= confirm("Do you want any lowercase letters in your password? \n Press OK if you DO and Cancel if you do not.");
-  userResponse["lowercase"] = wantLowercase;
-  //ask if they want any numeric values?
-  var wantNumeric= confirm("Do you want any numeric digits in your password? \n Press OK if you DO and Cancel if you do not.");
-  userResponse["numeric"] = wantNumeric;
-  //ask if they want any special characters?
-  var wantSpecialChar= confirm("Do you want any special characters($@%&*, etc) in your password? \n Press OK if you DO and Cancel if you do not.");
-  userResponse["specialChars"] = wantSpecialChar;
-
-  console.log(userResponse)
   return userResponse
 }
 // Function for getting a random element from an array
@@ -130,29 +140,50 @@ function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 
 }
+function checkSizeAndAdd(arr,item,size){
+    arr.unshift(item);
+    return true
 
+};
 // Function to generate password with user input
 function generatePassword() {
   var userResponse = getPasswordOptions();
-  var password = []
-  while (password.length < userResponse.length){
-    if(userResponse.uppercase){
-      password.unshift(getRandom(upperCasedCharacters));
-    };
-    if(userResponse.lowercase){
-      password.push(getRandom(lowerCasedCharacters));
-    };
-    if(userResponse.specialChars){
-      password.unshift(getRandom(specialCharacters));
-    };
-    if(userResponse.numeric){
-      password.unshift(getRandom(numericCharacters));
-    };
+  
+  var maxLength = userResponse.length
+  password =[]
+  var i = 0;
+  while(password.length < maxLength){
 
+    if (userResponse.uppercase ==true && password.length < maxLength  ){
+      password.push(getRandom(upperCasedCharacters));
+      i++
+
+    };
+    if (userResponse.lowercase ==true && password.length < maxLength  ){
+      password.unshift(getRandom(lowerCasedCharacters));
+      i++
+
+    };
+    if (userResponse.numeric ==true && password.length < maxLength  ){
+      password.unshift(getRandom(numericCharacters));
+      i++
+
+    };
+    if (userResponse.specialChars ==true && password.length < maxLength  ){
+      password.push(getRandom(specialCharacters));
+      i++
+
+    };
+    
   }
   return password.join("");
 
-}
+ 
+ 
+ 
+
+  };
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -161,10 +192,10 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
+  
 
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-writePassword();
+
